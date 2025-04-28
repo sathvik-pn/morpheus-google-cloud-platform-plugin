dc85d08e4ce79e79e70d3c3b1a2458ffc5e88bd5
sathvik-pn <sathvik-pn@hpe.com>
2025-04-28 11:38:15 +0530

    wip: configure UI to load local creds

--- src/main/groovy/com/morpheusdata/googlecloudplatform/GoogleCloudPlatformCloudProvider.groovy
+++ src/main/groovy/com/morpheusdata/googlecloudplatform/GoogleCloudPlatformCloudProvider.groovy
@@ -100,7 +100,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 				defaultValue: 'local',
 				displayOrder: displayOrderStart,
 				optionSource: 'credentials',
-				config: '{"credentialTypes":["email-private-key"]}'
+				config: '{"credentialTypes":["email-private-key", "client-id-secret"]}'
 		)
 
 		options << new OptionType(
@@ -112,6 +112,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 				fieldLabel: 'Client Email',
 				fieldCode: 'gomorpheus.label.email',
 				required: true,
+				localCredential: true, // expose only when local credential is selected
 				inputType: OptionType.InputType.TEXT,
 		)
 
@@ -124,6 +125,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 				fieldLabel: 'Private Key',
 				fieldCode: 'gomorpheus.label.private-key',
 				required: true,
+				localCredential: true, // expose only when local credential is selected
 				inputType: OptionType.InputType.PASSWORD,
 		)
 
@@ -200,6 +202,11 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 		return serverTypes
 	}
 
+
+	/*
+		[http-nio-8080-exec-8] SPN Plugin ServiceResponse validate(Cloud cloudInfo, ValidateCloudRequest validateCloudRequest) called. cloudInfo.getConfigMap = [email:487639795848-compute@developer.gserviceaccount.com, private-key:-----BEGIN PRIVATE KEY----- MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDHnGEHYmKbQmy7 Vh+WwPr5zVi+GKu0ybx3VwpyrOPGcUEjwKeBiOIE3e6XZSIPI0DcJnRj3t//Zti+ mG8Y3+Jzji6I9ahiVboGTpPmBDUS3YH8O7vq0gj5PsWLVGqeIgxmr9gDLKkNRjrT H3IDPjzOB1J7zYomn0NQefS+eS8ZQY26s+5LzJa6ElXafmO2YEq0zMeAuq7bst+/ YZ+4YBiZGkbrn85DlwavFDPuMTZUFXMsHEZbHVQdvVqK45AScBKi8JbHTAsbubdm AdTCdm8m1x+TpVy7GpQbYLpVW/NZJNiEtmuXpQqq9qGH7pQbVaQjcYnb4TUW3qt8 Fl6mhLRVAgMBAAECggEAHclAk10DkN6HIidWXGUcrnUNhoRg7v/hjz9eUEFrVdvl mAOyGzrFW2uQpGfTfsXIcnGMkbCa5V4//qT5gxE5lfvEBuz9fPsE4NcN69d54gBK 2sxBHOfNrf/Hc6SbqlAzYIlh5wtdiJ0Pvxxko4ZAhZADJ3VC5cODRHmGY3vq4s4p W0jtLVbc0EpQsqGuK3gfvsGDbQTx3iKxxTi93ETa9eqgPgO29rngJtfqaucz/eEj 6U+HY419GRIhUy9mYlM7EQ5EUgYoGEpMV1kMQobUOLIG7MzzDuDNFH9O4eWNvdw8 /gZJzMNWznqzm9HTLaVsaR1hdituUOGsFSjNTIw9AQKBgQDum25X6JqYe6WzUQOw kwIjR/p43zfgKm1icYRsxQySiGB82Z/aJu7thw40B7sUnmTAD/bljmXsxCU0MYaE /0ERQpZT+VmYcpRwYxv5ASJIfUf0gCTSpEnA3tcHXfZAto4ImGDDYLyzlG5ZSEJH VPmEa1RwO0H+OtCVuszNzZBXgQKBgQDWKUBC6hyQJs8j2pRYdNYvGSBPexYR+peY xCiWVPzfAiSMhiYy864wCuZHcoGgUEoTz1FFptqzIaXNzzvIhs4qZtKdKRwbFhW5 hQpwMOt5l1IAh5aVld7YGP3+lkS1/PC1ZhctO4zmwNdPFJZMSpQJdFiqEmrExWl7 z7AXBc7m1QKBgHvOMGnyqkyWU7sAPU7gaIqP7XnMMLziptEen5ykfcqGcrI1ZUkX TH/4xLjgjgS7zwENB4nC4kYA3GEBlY/qtgNTrpax2fhM85KBCmGgYf6E6tIFr5WQ YyhtN9t9uKmJT5dMIWEfD6qKSNuQr7s9bx5zZmO5i5tzPwmB3ISotc2BAoGBAM5X YHsYZfzd56Z764Ju0A8A/oiV52XbNEUUeqadrCaSQfTDK/rbnnDbj5Q3V33G3hjI H14krYL6YG4zxT4n5GwmNbcG7PWMCdJ9xNjjyX2VtoScHxJxHAzJx6+LyRgvZRzi CGKkhdWu58Rh6Y4ILSoO137Fz+lW65NHQDpudjMhAoGBALQKssHWW5Rjp7AVilOB rSNtRxRUigrEFh4YlSgQtlS4w5V1Y8wTVhxEEohadR0DEYVVBLlWwMqyTWl/2RXU xSIq4LxGLYSxh9yVbaJngLIJHoATJ10Jzs67lT7ra+Oh3ru9rnny99GwP69pMwp9 66ZtJFGtIQC58jXzi6yqBkFr -----END PRIVATE KEY-----, applianceUrl:1.1.1.1, datacenterName:1, networkServer.id:unmanaged, networkServer:[id:unmanaged], securityServer:off, backupMode:internal, replicationMode:-1] \n\n validateCloudRequest.opts = [stepIndex:2, cloudfilter:, zoneType:google-cloud-platform-plugin.cloud, zone.zoneType.id:1, zone:[zoneType.id:1, zoneType:[id:1, code:google-cloud-platform-plugin.cloud], zoneType.code:google-cloud-platform-plugin.cloud, name:spn-cloud, code:, labelString:, location:, _enabled:, enabled:on, _autoRecoverPowerState:, autoRecoverPowerState:on, networkDomain.id:1, networkDomain:[id:1], timezone:GMT, securityMode:internal, guidanceMode:off, costingMode:costing, agentMode:cloudInit, _defaultDatastoreSyncActive:, defaultDatastoreSyncActive:on, _defaultNetworkSyncActive:, defaultNetworkSyncActive:on, _applianceUrlProxyBypass:, applianceUrlProxyBypass:*******, noProxy:, userDataLinux:my-user-data-linux], zone.zoneType.code:google-cloud-platform-plugin.cloud, zone.name:spn-cloud, zone.code:, zone.labelString:, zone.location:, zone._enabled:, zone.enabled:on, zone._autoRecoverPowerState:, zone.autoRecoverPowerState:on, credential.type:local, credential:[type:local, integration.id:, integration:[id:]], credential.integration.id:, config.email:487639795848-compute@developer.gserviceaccount.com, config:[email:487639795848-compute@developer.gserviceaccount.com, private-key:-----BEGIN PRIVATE KEY----- MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDHnGEHYmKbQmy7 Vh+WwPr5zVi+GKu0ybx3VwpyrOPGcUEjwKeBiOIE3e6XZSIPI0DcJnRj3t//Zti+ mG8Y3+Jzji6I9ahiVboGTpPmBDUS3YH8O7vq0gj5PsWLVGqeIgxmr9gDLKkNRjrT H3IDPjzOB1J7zYomn0NQefS+eS8ZQY26s+5LzJa6ElXafmO2YEq0zMeAuq7bst+/ YZ+4YBiZGkbrn85DlwavFDPuMTZUFXMsHEZbHVQdvVqK45AScBKi8JbHTAsbubdm AdTCdm8m1x+TpVy7GpQbYLpVW/NZJNiEtmuXpQqq9qGH7pQbVaQjcYnb4TUW3qt8 Fl6mhLRVAgMBAAECggEAHclAk10DkN6HIidWXGUcrnUNhoRg7v/hjz9eUEFrVdvl mAOyGzrFW2uQpGfTfsXIcnGMkbCa5V4//qT5gxE5lfvEBuz9fPsE4NcN69d54gBK 2sxBHOfNrf/Hc6SbqlAzYIlh5wtdiJ0Pvxxko4ZAhZADJ3VC5cODRHmGY3vq4s4p W0jtLVbc0EpQsqGuK3gfvsGDbQTx3iKxxTi93ETa9eqgPgO29rngJtfqaucz/eEj 6U+HY419GRIhUy9mYlM7EQ5EUgYoGEpMV1kMQobUOLIG7MzzDuDNFH9O4eWNvdw8 /gZJzMNWznqzm9HTLaVsaR1hdituUOGsFSjNTIw9AQKBgQDum25X6JqYe6WzUQOw kwIjR/p43zfgKm1icYRsxQySiGB82Z/aJu7thw40B7sUnmTAD/bljmXsxCU0MYaE /0ERQpZT+VmYcpRwYxv5ASJIfUf0gCTSpEnA3tcHXfZAto4ImGDDYLyzlG5ZSEJH VPmEa1RwO0H+OtCVuszNzZBXgQKBgQDWKUBC6hyQJs8j2pRYdNYvGSBPexYR+peY xCiWVPzfAiSMhiYy864wCuZHcoGgUEoTz1FFptqzIaXNzzvIhs4qZtKdKRwbFhW5 hQpwMOt5l1IAh5aVld7YGP3+lkS1/PC1ZhctO4zmwNdPFJZMSpQJdFiqEmrExWl7 z7AXBc7m1QKBgHvOMGnyqkyWU7sAPU7gaIqP7XnMMLziptEen5ykfcqGcrI1ZUkX TH/4xLjgjgS7zwENB4nC4kYA3GEBlY/qtgNTrpax2fhM85KBCmGgYf6E6tIFr5WQ YyhtN9t9uKmJT5dMIWEfD6qKSNuQr7s9bx5zZmO5i5tzPwmB3ISotc2BAoGBAM5X YHsYZfzd56Z764Ju0A8A/oiV52XbNEUUeqadrCaSQfTDK/rbnnDbj5Q3V33G3hjI H14krYL6YG4zxT4n5GwmNbcG7PWMCdJ9xNjjyX2VtoScHxJxHAzJx6+LyRgvZRzi CGKkhdWu58Rh6Y4ILSoO137Fz+lW65NHQDpudjMhAoGBALQKssHWW5Rjp7AVilOB rSNtRxRUigrEFh4YlSgQtlS4w5V1Y8wTVhxEEohadR0DEYVVBLlWwMqyTWl/2RXU xSIq4LxGLYSxh9yVbaJngLIJHoATJ10Jzs67lT7ra+Oh3ru9rnny99GwP69pMwp9 66ZtJFGtIQC58jXzi6yqBkFr -----END PRIVATE KEY-----, applianceUrl:1.1.1.1, datacenterName:1, networkServer.id:unmanaged, networkServer:[id:unmanaged], securityServer:off, backupMode:internal, replicationMode:-1], config.private-key:-----BEGIN PRIVATE KEY----- MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDHnGEHYmKbQmy7 Vh+WwPr5zVi+GKu0ybx3VwpyrOPGcUEjwKeBiOIE3e6XZSIPI0DcJnRj3t//Zti+ mG8Y3+Jzji6I9ahiVboGTpPmBDUS3YH8O7vq0gj5PsWLVGqeIgxmr9gDLKkNRjrT H3IDPjzOB1J7zYomn0NQefS+eS8ZQY26s+5LzJa6ElXafmO2YEq0zMeAuq7bst+/ YZ+4YBiZGkbrn85DlwavFDPuMTZUFXMsHEZbHVQdvVqK45AScBKi8JbHTAsbubdm AdTCdm8m1x+TpVy7GpQbYLpVW/NZJNiEtmuXpQqq9qGH7pQbVaQjcYnb4TUW3qt8 Fl6mhLRVAgMBAAECggEAHclAk10DkN6HIidWXGUcrnUNhoRg7v/hjz9eUEFrVdvl mAOyGzrFW2uQpGfTfsXIcnGMkbCa5V4//qT5gxE5lfvEBuz9fPsE4NcN69d54gBK 2sxBHOfNrf/Hc6SbqlAzYIlh5wtdiJ0Pvxxko4ZAhZADJ3VC5cODRHmGY3vq4s4p W0jtLVbc0EpQsqGuK3gfvsGDbQTx3iKxxTi93ETa9eqgPgO29rngJtfqaucz/eEj 6U+HY419GRIhUy9mYlM7EQ5EUgYoGEpMV1kMQobUOLIG7MzzDuDNFH9O4eWNvdw8 /gZJzMNWznqzm9HTLaVsaR1hdituUOGsFSjNTIw9AQKBgQDum25X6JqYe6WzUQOw kwIjR/p43zfgKm1icYRsxQySiGB82Z/aJu7thw40B7sUnmTAD/bljmXsxCU0MYaE /0ERQpZT+VmYcpRwYxv5ASJIfUf0gCTSpEnA3tcHXfZAto4ImGDDYLyzlG5ZSEJH VPmEa1RwO0H+OtCVuszNzZBXgQKBgQDWKUBC6hyQJs8j2pRYdNYvGSBPexYR+peY xCiWVPzfAiSMhiYy864wCuZHcoGgUEoTz1FFptqzIaXNzzvIhs4qZtKdKRwbFhW5 hQpwMOt5l1IAh5aVld7YGP3+lkS1/PC1ZhctO4zmwNdPFJZMSpQJdFiqEmrExWl7 z7AXBc7m1QKBgHvOMGnyqkyWU7sAPU7gaIqP7XnMMLziptEen5ykfcqGcrI1ZUkX TH/4xLjgjgS7zwENB4nC4kYA3GEBlY/qtgNTrpax2fhM85KBCmGgYf6E6tIFr5WQ YyhtN9t9uKmJT5dMIWEfD6qKSNuQr7s9bx5zZmO5i5tzPwmB3ISotc2BAoGBAM5X YHsYZfzd56Z764Ju0A8A/oiV52XbNEUUeqadrCaSQfTDK/rbnnDbj5Q3V33G3hjI H14krYL6YG4zxT4n5GwmNbcG7PWMCdJ9xNjjyX2VtoScHxJxHAzJx6+LyRgvZRzi CGKkhdWu58Rh6Y4ILSoO137Fz+lW65NHQDpudjMhAoGBALQKssHWW5Rjp7AVilOB rSNtRxRUigrEFh4YlSgQtlS4w5V1Y8wTVhxEEohadR0DEYVVBLlWwMqyTWl/2RXU xSIq4LxGLYSxh9yVbaJngLIJHoATJ10Jzs67lT7ra+Oh3ru9rnny99GwP69pMwp9 66ZtJFGtIQC58jXzi6yqBkFr -----END PRIVATE KEY-----, zone.networkDomain.id:1, config.applianceUrl:1.1.1.1, zone.timezone:GMT, config.datacenterName:1, config.networkServer.id:unmanaged, zone.securityMode:internal, config.securityServer:off, config.backupMode:internal, config.replicationMode:-1, zone.guidanceMode:off, zone.costingMode:costing, zone.agentMode:cloudInit, zone._defaultDatastoreSyncActive:, zone.defaultDatastoreSyncActive:on, zone._defaultNetworkSyncActive:, zone.defaultNetworkSyncActive:on, zone._applianceUrlProxyBypass:, zone.applianceUrlProxyBypass:*******, zone.noProxy:, zone.userDataLinux:my-user-data-linux, controller:siteZone, action:step, user:Sathvik PN[sathvikpn - sathvikpn@hpe.com]] \n\n cloudInfo.account = com.morpheusdata.model.Account@43b822a4 cloudType = com.morpheusdata.model.CloudType@2a75b4e8 type = null serviceUrl = null serviceUsername = null servicePassword = null serviceToken = null \n\n accountCredentialData = null accountCredentialLoaded = false validateCloudRequest.credentialUsername = null validateCloudRequest.credentialPassword = null validateCloudRequest.credentialType = local
+	 */
+
 	/**
 	 * Validates the submitted cloud information to make sure it is functioning correctly.
 	 * If a {@link ServiceResponse} is not marked as successful then the validation results will be
@@ -211,9 +218,9 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	@Override
 	ServiceResponse validate(Cloud cloudInfo, ValidateCloudRequest validateCloudRequest) {
 		def debug_log = """
-		SPN ServiceResponse validate(Cloud cloudInfo, ValidateCloudRequest validateCloudRequest) called. 
-		cloudInfo.getConfigMap = ${cloudInfo.getConfigMap()}                      
-		validateCloudRequest.opts = ${validateCloudRequest.opts}
+		SPN Plugin ServiceResponse validate(Cloud cloudInfo, ValidateCloudRequest validateCloudRequest) called. \n\n
+		cloudInfo.getConfigMap = ${cloudInfo.getConfigMap()}                      \\n\\n
+		validateCloudRequest.opts = ${validateCloudRequest.opts}  \\n\\n
 
 		cloudInfo.account = ${cloudInfo.account}
 		cloudType = ${cloudInfo.cloudType}
@@ -221,35 +228,59 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 		serviceUrl = ${cloudInfo.serviceUrl}
 		serviceUsername = ${cloudInfo.serviceUsername}
 		servicePassword = ${cloudInfo.servicePassword}
-		serviceToken = ${cloudInfo.serviceToken} 
+		serviceToken = ${cloudInfo.serviceToken}   \\n\\n
 
 		accountCredentialData = ${cloudInfo.accountCredentialData}
 		accountCredentialLoaded = ${cloudInfo.accountCredentialLoaded}
 
+		validateCloudRequest.credentialUsername = ${validateCloudRequest.credentialUsername}
+		validateCloudRequest.credentialPassword = ${validateCloudRequest.credentialPassword}
+		validateCloudRequest.credentialType = ${validateCloudRequest.credentialType}
+
 		"""
 		log.info(debug_log)
-//		cloudInfo.account = com.morpheusdata.model.Account@391339af cloudType = com.morpheusdata.model.CloudType@4ac9141 type = null serviceUrl = null serviceUsername = null servicePassword = null serviceToken = null accountCredentialData = null accountCredentialLoaded = false
+//		cloudInfo.account = com.morpheusdata.model.Account@391339af 
+// cloudType = com.morpheusdata.model.CloudType@4ac9141 type = null serviceUrl = null serviceUsername = null servicePassword = null serviceToken = null 
+// accountCredentialData = null accountCredentialLoaded = false
 		try {
 			if (cloudInfo) {
 				String email = ""
 				String privateKey = ""
 
-				email = validateCloudRequest.opts?.config['email']
-				privateKey = validateCloudRequest.opts?.config['private-key']
+				def cloudConfig = cloudInfo.getConfigMap() 
 
-				log.info("SPN Plugin retrieved email: ${email} and privateKey: ${privateKey}")
-				// SPN retrieved email: email-is-spn@email.com and privateKey: pvtkeyisspn123
+				// email = validateCloudRequest.opts?.config['email']
+				// privateKey = validateCloudRequest.opts?.config['private-key']
+
+				//
+				if(validateCloudRequest.credentialType == 'email-private-key') {
+					email = validateCloudRequest.credentialUsername
+					privateKey = validateCloudRequest.credentialPassword
+					log.info("SPN Plugin email-private-key creds from validateCloudRequest: email==${email} and pkey==${privateKey}")
+				} else if (validateCloudRequest.credentialType == 'local') {
+					email = validateCloudRequest.opts?.config['email']
+					privateKey = validateCloudRequest.opts?.config['private-key']
+					log.info("SPN Plugin local creds from validateCloudRequest: email==${email} and pkey==${privateKey}")
+
+					email = cloudConfig?.email
+					privateKey = cloudConfig?.'private-key'
+					log.info("SPN Plugin local creds from cloudInfoConfigMap: email==${email} and pkey==${privateKey}")
+				}
 
 				if (email?.isBlank()) {
 					return new ServiceResponse(success: false, msg: 'Enter the email')
 				} else if (privateKey?.isBlank()) {
 					return new ServiceResponse(success: false, msg: 'Enter the private key')
 				} else {
-//					RequestConfig reqConfig = [
-//							email  : email,
-//							privateKey: privateKey,
-//					]
+					cloudInfo.accountCredentialData = [
+							'email': email,
+							'private-key': privateKey,
+					]
+					cloudInfo.accountCredentialLoaded = true
 
+					log.info("SPN Plugin accountCredentialData: ${cloudInfo.accountCredentialData}")
+					log.info("SPN Plugin accountCredentialLoaded: ${cloudInfo.accountCredentialLoaded}")
+					// Call the API to validate the credentials
 					RequestConfig requestConfig = new RequestConfig(
 							email: email,
 							privateKey: privateKey,
@@ -257,14 +288,12 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 					HttpApiClient apiClient = new HttpApiClient()
 					apiClient.networkProxy = cloudInfo.apiProxy
 					try {
-//						def projectList = NutanixPrismElementApiService.listContainers(apiClient, new RequestConfig(reqConfig))
-
 						log.info("SPN Plugin projectList API Call Placeholder")
 						def projectList = GoogleCloudPlatformApiService.listProjects(apiClient, requestConfig)
 						if (projectList.success == true) {
-							return new ServiceResponse(success: true, msg: 'Plugin validation successful (api call)', data: projectList)
+							return new ServiceResponse(success: true, msg: 'SPN Plugin validation successful (api call)', data: projectList)
 						} else {
-							return new ServiceResponse(success: false, msg: 'Plugin invalid credentials')
+							return new ServiceResponse(success: false, msg: 'SPN Plugin invalid credentials')
 						}
 					} finally {
 						apiClient.shutdownClient()
@@ -287,8 +316,8 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	ServiceResponse initializeCloud(Cloud cloudInfo) {
-		log.info("SPN ServiceResponse initializeCloud(Cloud cloudInfo)")
-		log.info("SPN initializeCloud: ${cloudInfo}")
+		log.info("SPN Plugin ServiceResponse initializeCloud(Cloud cloudInfo)")
+		log.info("SPN Plugin initializeCloud: ${cloudInfo}")
 
 		if (cloudInfo) {
 			if (cloudInfo.enabled) {
@@ -311,6 +340,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	ServiceResponse refresh(Cloud cloudInfo) {
+		log.info("SPN Plugin refresh(Cloud cloudInfo) called. cloud = ${cloudInfo}")
 		return ServiceResponse.success()
 	}
 
@@ -322,6 +352,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	void refreshDaily(Cloud cloudInfo) {
+		log.info("SPN Plugin refreshDaily(Cloud cloudInfo) called. cloud = ${cloudInfo}")
 	}
 
 	/**
@@ -331,6 +362,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	ServiceResponse deleteCloud(Cloud cloudInfo) {
+		log.info("SPN Plugin deleteCloud(Cloud cloudInfo) called. cloud = ${cloudInfo}")
 		return ServiceResponse.success()
 	}
 
@@ -386,7 +418,11 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	Boolean hasCloudInit() {
+		log.info("SPN Plugin hasCloudInit")
 		return true
+
+//		[http-nio-8080-exec-4] SPN Plugin hasCloudInit
+//		[http-nio-8080-exec-4] Google Cloud Platform Plugin initialized
 	}
 
 	/**
@@ -406,6 +442,7 @@ class GoogleCloudPlatformCloudProvider implements CloudProvider {
 	 */
 	@Override
 	ServiceResponse startServer(ComputeServer computeServer) {
+		log.info("SPN Plugin startServer")
 		return ServiceResponse.success()
 	}
 
